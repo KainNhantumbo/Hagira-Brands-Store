b97901331999888a9e5dfad14e96114dbc929b79
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-20 17:34:13 +0200

    feat: added project primary colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { Container } from './styles/app';
 
-function App() {
+const App = () => {
 	return (
 		<section>
 			<Header />
 		</section>
 	);
-}
+};
 
 export default App;
---
+++ src/styles/themes.js
@@ -0,0 +1,6 @@
+export const primary = {
+	primary: '0, 174, 239',
+	secondary: '128, 102, 149',
+	brigther: '223, 235, 248',
+	dark: '76, 127, 148',
+};
