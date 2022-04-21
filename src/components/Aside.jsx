1ef789b40df201f2402384d2e26aca3833afcef6
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-21 18:46:45 +0200

    styles: added styles to Aside component.

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,23 +3,27 @@ import Button from './Button';
 import { BiSearch } from 'react-icons/bi';
 const Aside = () => {
 	const categories = {
-		category: ['All', 'Cozinha', 'Panos', 'Batas', 'Cortinas'],
+		category: ['Todos', 'Cozinha', 'Panos', 'Batas', 'Cortinas'],
 	};
 
 	return (
 		<AsideContainer>
 			<section>
-      <div>Pesquisa</div>
-				<div>
+				<div className='title'>Pesquisa</div>
+				<div className='search'>
 					<input type='search' placeholder='Digite algo aqui...' />
 					<Button text={'Buscar'} icon={<BiSearch />} />
 				</div>
 			</section>
 			<section>
-				<div>Categorias</div>
+				<div className='title'>Categorias e Tags</div>
 				<ul>
 					{categories.category.map((category, index) => {
-						return <li key={index}>{category}</li>;
+						return (
+							<li key={index} className='tags'>
+								<span>{category}</span>
+							</li>
+						);
 					})}
 				</ul>
 			</section>
--- src/styles/components/aside.js
+++ src/styles/components/aside.js
@@ -2,4 +2,54 @@ import styled from 'styled-components';
 
 export const AsideContainer = styled.aside`
 	width: 30%;
+	display: flex;
+	justify-content: flex-start;
+	flex-direction: column;
+	gap: 15px;
+
+	section {
+		ul {
+			display: flex;
+			justify-content: flex-start;
+			flex-flow: row wrap;
+			gap: 15px;
+
+			.tags {
+				span {
+					padding: 2px 8px;
+					border-radius: 12px;
+					font-size: 0.9rem;
+					color: rgb(${({ theme }) => theme.text});
+					background: rgb(${({ theme }) => theme.secondary});
+
+					:hover {
+						background: rgb(${({ theme }) => theme.primary});
+					}
+				}
+			}
+		}
+
+		.title {
+			margin-bottom: 12px;
+			font-weight: 500;
+		}
+
+		.search {
+			display: flex;
+			justify-content: flex-start;
+			gap: 5px;
+
+			input {
+				border: 2px solid rgb(${({ theme }) => theme.primary});
+				border-radius: 3px;
+				padding: 5px;
+
+				:focus {
+					box-shadow: 0 0 10px rgb(${({ theme }) => theme.primary});
+					border: 2px solid rgb(${({ theme }) => theme.primary});
+					outline-color: rgb(${({ theme }) => theme.primary});
+				}
+			}
+		}
+	}
 `;
