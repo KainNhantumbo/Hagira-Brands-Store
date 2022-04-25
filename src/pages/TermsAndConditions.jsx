1507a37911951a8b2812849dec2bb8d24ef5a774
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-25 14:54:20 +0200

    styles: added styles for terms and conditions page.

--- src/pages/TermsAndConditions.jsx
+++ src/pages/TermsAndConditions.jsx
@@ -9,21 +9,23 @@ const TermsAndConditions = () => {
 			<article>
 				<p>
 					Ao acessar ao site{' '}
-					<a href='hagira-brands.netlify.app'>Hagira Brands</a>, concorda em
-					cumprir estes termos de serviço, todas as leis e regulamentos
-					aplicáveis e concorda que é responsável pelo cumprimento de todas as
-					leis locais aplicáveis. Se você não concordar com algum desses termos,
-					está proibido de usar ou acessar este site. Os materiais contidos
-					neste site são protegidos pelas leis de direitos autorais e marcas
-					comerciais aplicáveis.
+					<a href='/'>
+						<strong>Hagira Brands</strong>
+					</a>
+					, concorda em cumprir estes termos de serviço, todas as leis e
+					regulamentos aplicáveis e concorda que é responsável pelo cumprimento
+					de todas as leis locais aplicáveis. Se você não concordar com algum
+					desses termos, está proibido de usar ou acessar este site. Os
+					materiais contidos neste site são protegidos pelas leis de direitos
+					autorais e marcas comerciais aplicáveis.
 				</p>
 				<h2>2. Uso de Licença</h2>
 				<p>
 					É concedida permissão para baixar temporariamente uma cópia dos
-					materiais (informações ou software) no site Hagira Brands , apenas
-					para visualização transitória pessoal e não comercial. Esta é a
-					concessão de uma licença, não uma transferência de título e, sob esta
-					licença, você não pode:{' '}
+					materiais (informações ou software) no site{' '}
+					<strong>Hagira Brands</strong> , apenas para visualização transitória
+					pessoal e não comercial. Esta é a concessão de uma licença, não uma
+					transferência de título e, sob esta licença, você não pode:{' '}
 				</p>
 				<ol>
 					<li>modificar ou copiar os materiais; </li>
@@ -33,7 +35,7 @@ const TermsAndConditions = () => {
 					</li>
 					<li>
 						tentar descompilar ou fazer engenharia reversa de qualquer software
-						contido no site Hagira Brands;{' '}
+						contido no site <strong>Hagira Brands</strong>;{' '}
 					</li>
 					<li>
 						remover quaisquer direitos autorais ou outras notações de
@@ -46,35 +48,39 @@ const TermsAndConditions = () => {
 				</ol>
 				<p>
 					Esta licença será automaticamente rescindida se você violar alguma
-					dessas restrições e poderá ser rescindida por Hagira Brands a qualquer
-					momento. Ao encerrar a visualização desses materiais ou após o término
-					desta licença, você deve apagar todos os materiais baixados em sua
-					posse, seja em formato eletrónico ou impresso.
+					dessas restrições e poderá ser rescindida por{' '}
+					<strong>Hagira Brands</strong> a qualquer momento. Ao encerrar a
+					visualização desses materiais ou após o término desta licença, você
+					deve apagar todos os materiais baixados em sua posse, seja em formato
+					eletrónico ou impresso.
 				</p>
+
 				<h2>3. Isenção de responsabilidade</h2>
-				<ol>
-					<li>
-						Os materiais no site da Hagira Brands são fornecidos 'como estão'.
-						Hagira Brands não oferece garantias, expressas ou implícitas, e, por
-						este meio, isenta e nega todas as outras garantias, incluindo, sem
-						limitação, garantias implícitas ou condições de comercialização,
-						adequação a um fim específico ou não violação de propriedade
-						intelectual ou outra violação de direitos.{' '}
-					</li>
-					<li>
-						Além disso, o Hagira Brands não garante ou faz qualquer
-						representação relativa à precisão, aos resultados prováveis ou à
-						confiabilidade do uso dos materiais em seu site ou de outra forma
-						relacionado a esses materiais ou em sites vinculados a este site.
-					</li>
-				</ol>
+				<p>
+					Os materiais no site da <strong>Hagira Brands</strong> são fornecidos
+					'como estão'.
+					<strong> Hagira Brands</strong> não oferece garantias, expressas ou
+					implícitas, e, por este meio, isenta e nega todas as outras garantias,
+					incluindo, sem limitação, garantias implícitas ou condições de
+					comercialização, adequação a um fim específico ou não violação de
+					propriedade intelectual ou outra violação de direitos.{' '}
+				</p>
+				<p>
+					Além disso, o <strong>Hagira Brands</strong> não garante ou faz
+					qualquer representação relativa à precisão, aos resultados prováveis
+					ou à confiabilidade do uso dos materiais em seu site ou de outra forma
+					relacionado a esses materiais ou em sites vinculados a este site.
+				</p>
+
 				<h2>4. Limitações</h2>
 				<p>
-					Em nenhum caso o Hagira Brands ou seus fornecedores serão responsáveis
-					por quaisquer danos (incluindo, sem limitação, danos por perda de
-					dados ou lucro ou devido a interrupção dos negócios) decorrentes do
-					uso ou da incapacidade de usar os materiais em Hagira Brands, mesmo
-					que Hagira Brands ou um representante autorizado da Hagira Brands
+					Em nenhum caso o <strong>Hagira Brands</strong> ou seus fornecedores
+					serão responsáveis por quaisquer danos (incluindo, sem limitação,
+					danos por perda de dados ou lucro ou devido a interrupção dos
+					negócios) decorrentes do uso ou da incapacidade de usar os materiais
+					em <strong>Hagira Brands</strong>, mesmo que{' '}
+					<strong>Hagira Brands</strong> ou um representante autorizado da{' '}
+					<strong>Hagira Brands </strong>
 					tenha sido notificado oralmente ou por escrito da possibilidade de
 					tais danos. Como algumas jurisdições não permitem limitações em
 					garantias implícitas, ou limitações de responsabilidade por danos
@@ -83,31 +89,41 @@ const TermsAndConditions = () => {
 				</p>
 				<h2>5. Precisão dos materiais</h2>
 				<p>
-					Os materiais exibidos no site da Hagira Brands podem incluir erros
-					técnicos, tipográficos ou fotográficos. Hagira Brands não garante que
-					qualquer material em seu site seja preciso, completo ou atual. Hagira
-					Brands pode fazer alterações nos materiais contidos em seu site a
-					qualquer momento, sem aviso prévio. No entanto, Hagira Brands não se
+					Os materiais exibidos no site da <strong>Hagira Brands</strong> podem
+					incluir erros técnicos, tipográficos ou fotográficos.{' '}
+					<strong>Hagira Brands</strong> não garante que qualquer material em
+					seu site seja preciso, completo ou atual. Hagira Brands pode fazer
+					alterações nos materiais contidos em seu site a qualquer momento, sem
+					aviso prévio. No entanto, <strong>Hagira Brands</strong> não se
 					compromete a atualizar os materiais.
 				</p>
 				<h2>6. Links</h2>
 				<p>
-					O Hagira Brands não analisou todos os sites vinculados ao seu site e
-					não é responsável pelo conteúdo de nenhum site vinculado. A inclusão
-					de qualquer link não implica endosso por Hagira Brands do site. O uso
-					de qualquer site vinculado é por conta e risco do usuário.
+					O <strong>Hagira Brands</strong> não analisou todos os sites
+					vinculados ao seu site e não é responsável pelo conteúdo de nenhum
+					site vinculado. A inclusão de qualquer link não implica endosso por{' '}
+					<strong>Hagira Brands</strong> do site. O uso de qualquer site
+					vinculado é por conta e risco do usuário.
 				</p>
 				<h3>Modificações</h3>
 				<p>
-					O Hagira Brands pode revisar estes termos de serviço do site a
-					qualquer momento, sem aviso prévio. Ao usar este site, você concorda
-					em ficar vinculado à versão atual desses termos de serviço.
+					O{' '}
+					<strong>
+						<strong>Hagira Brands</strong>
+					</strong>{' '}
+					pode revisar estes termos de serviço do site a qualquer momento, sem
+					aviso prévio. Ao usar este site, você concorda em ficar vinculado à
+					versão atual desses termos de serviço.
 				</p>
 				<h3>Lei aplicável</h3>
 				<p>
 					Estes termos e condições são regidos e interpretados de acordo com as
-					leis do Hagira Brands e você se submete irrevogavelmente à jurisdição
-					exclusiva dos tribunais naquele estado ou localidade.
+					leis da{' '}
+					<strong>
+						<strong>Hagira Brands</strong>
+					</strong>{' '}
+					e você se submete irrevogavelmente à jurisdição exclusiva dos
+					tribunais nosso estado e localidade.
 				</p>
 			</article>
 		</TermsAndConditionsContainer>
--- src/styles/termsAndConditions.js
+++ src/styles/termsAndConditions.js
@@ -1,3 +1,42 @@
-import styled from "styled-components"; 
+import styled from 'styled-components';
 
-export const TermsAndConditionsContainer = styled.main``
+export const TermsAndConditionsContainer = styled.main`
+	margin: 50px 0;
+  padding: 30px;
+
+	section {
+		h1 {
+			font-size: 1.8rem;
+			font-weight: 500;
+		}
+	}
+
+	article {
+		line-height: 1.6rem;
+
+    h2 {
+			color: rgb(${({ theme }) => theme.secondary});
+      font-weight: 500;
+      font-size: 1.2rem;
+    }
+
+    h3 {
+      font-weight: 500;
+			color: rgb(${({ theme }) => theme.secondary});
+    }
+
+		p {
+			font-size: 1rem;
+      padding: 10px 0;
+		}
+
+    li {
+      list-style: circle;
+      list-style-position: inside;
+    }
+
+		a {
+			color: rgb(${({ theme }) => theme.primary});
+		}
+	}
+`;
