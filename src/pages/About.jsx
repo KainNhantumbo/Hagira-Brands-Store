fdf21cceb834d74bc08c9f8c4a9f632d386950b5
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-29 14:43:18 +0200

    feat: added items and styles to About page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,47 @@
-import { AboutContainer } from "../styles/about";
-import aboutImage from '../img/about0.jpg'
+import { AboutContainer } from '../styles/about';
+import aboutImage from '../img/about0.jpg';
+import { BiPlanet } from 'react-icons/bi';
 
 const About = () => {
-  return <AboutContainer>
-    <img src={aboutImage} alt="" />
-  </AboutContainer>;
-}
- 
+	return (
+		<AboutContainer>
+			<h1>
+				Sobre
+				<BiPlanet />
+			</h1>
+			<div>
+				<section className='image-container'>
+					<figure>
+						<img src={aboutImage} alt='Imagem da Autora' />
+						<figcaption>Foto da Autora</figcaption>
+					</figure>
+				</section>
+				<section className='about-content'>
+					<h3>Minha História</h3>
+					<p>
+						Certo dia, recebi uma máquina de costura e disse para mim mesma:
+						gostaria de fazer diferente do que todos fazem, algo que me
+						empolgue.
+					</p>
+					<p>
+						Implementei a ideia que tive de começar a fazer roupas e deu certo.
+						Sinto que que sou uma guerreira por gostar de aprender e fazer
+						acontecer com a graça de Deus.
+					</p>
+
+					<h3>Visão</h3>
+					<p>
+						Pretendo com os meus trabalhos alcançar e satisfazer a demanda do
+						mercado, fazendo produtos de qualidade, com base em matéria-prima
+						durável e de qualidade para todas as áreas envolvidas no contexto
+						vestuário (uniformes, fardas, roupas temáticas, roupas de cozinha,
+						panos, etc.) e bordados, levando o nome da minha marca,{' '}
+						<strong>Hagira Brands</strong> além.
+					</p>
+				</section>
+			</div>
+		</AboutContainer>
+	);
+};
+
 export default About;
--- src/styles/about.js
+++ src/styles/about.js
@@ -2,14 +2,27 @@ import styled from 'styled-components';
 
 export const AboutContainer = styled.main`
 	width: 100%;
-	max-width: 750px;
+	max-width: 850px;
 	margin: 0 auto;
 	padding: 80px 10px;
 	display: flex;
-	flex-direction: column;
-	gap: 20px;
+	flex-flow: column wrap;
+	gap: 10px;
 
 	line-height: 1.6rem;
+
+	div {
+		display: flex;
+		flex-flow: row wrap;
+		gap: 30px;
+
+    @media screen and (max-width: 780px) {
+      flex-direction: column;
+      justify-content: center;
+      align-content: center;
+      place-items: center;
+    }
+	}
 
 	i {
 		font-style: italic;
@@ -19,11 +32,62 @@ export const AboutContainer = styled.main`
 		font-size: 1.8rem;
 		font-weight: 500;
 		position: relative;
+
+		svg {
+			position: absolute;
+			left: 90px;
+			top: -2px;
+			width: 30px;
+			height: 30px;
+		}
+	}
+
+	a {
+		color: rgb(${({ theme }) => theme.primary});
 	}
 
 	h2,
 	h3,
 	label {
 		font-weight: 500;
+	}
+
+	h3 {
+		position: relative;
+		padding-left: 20px;
+
+		svg {
+			color: rgb(${({ theme }) => theme.primary});
+			position: absolute;
+			width: 18px;
+			height: 18px;
+			top: 3px;
+			left: 0;
+		}
+	}
+
+	.image-container {
+		img {
+			max-width: 300px;
+			width: 100%;
+			border-radius: 10px;
+
+      @media screen and (max-width: 780px) {
+        max-width: 280px;
+        
+      }
+		}
+	}
+
+	.about-content {
+		display: flex;
+		justify-content: flex-start;
+		flex-direction: column;
+		gap: 10px;
+		max-width: 420px;
+
+		p {
+			text-align: justify;
+		}
 	}
 `;
