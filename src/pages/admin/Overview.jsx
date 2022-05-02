0950490fcdc04e836ec416a729e65d0ad17f05e8
Kain Nhantumbo <nhantumbok@gmail.com>
2022-05-02 03:40:48 +0200

    feat: added support for getting data records from  server on Overview component.

--- src/pages/admin/Overview.jsx
+++ src/pages/admin/Overview.jsx
@@ -1,15 +1,44 @@
 import { OverviewContainer } from '../../styles/admin/overview';
-import {
-	BiStats,
-	BiMessageDetail,
-	BiStore,
-} from 'react-icons/bi';
-import React, { useState } from 'react';
+import { BiStats, BiMessageDetail, BiStore } from 'react-icons/bi';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Overview = () => {
 	const [visitors, setVisitors] = useState(0);
 	const [messages, setMessages] = useState(0);
 	const [produts, setProduts] = useState(0);
+
+	const server_getAmountOfProducts_url =
+		'http://localhost:4630/api/v1/products';
+	const server_getAmountOfVisitors_url =
+		'http://localhost:4630/api/v1/visitors';
+	const server_getAmountOfMessages_url =
+		'http://localhost:4630/api/v1/messages';
+
+	const fetchData = async () => {
+		try {
+			const response_messages = await axios({
+				method: 'get',
+				url: server_getAmountOfMessages_url,
+			});
+			const response_visitors = await axios({
+				method: 'get',
+				url: server_getAmountOfVisitors_url,
+			});
+			const response_products = await axios({
+				method: 'get',
+				url: server_getAmountOfProducts_url,
+			});
+			setMessages(() => response_messages.data.results);
+			setVisitors(() => response_visitors.data.results);
+			setProduts(() => response_products.data.results);
+			console.log(response_messages.data);
+		} catch (err) {}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
 
 	return (
 		<OverviewContainer>
