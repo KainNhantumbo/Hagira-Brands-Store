8c4bb51ddfa87c58cb474933eacde8345e6e9023
Kain Nhantumbo <nhantumbok@gmail.com>
2022-05-03 22:39:51 +0200

    feat: added a empty message when there are no products in Products at Admin page component

--- src/pages/admin/Products.jsx
+++ src/pages/admin/Products.jsx
@@ -1,9 +1,40 @@
 import { ProductsContainer } from '../../styles/admin/products';
+import React, { useState, useEffect } from 'react';
+import { FaArchive } from 'react-icons/fa';
+import { BiCollection } from 'react-icons/bi';
 
 const Products = () => {
-	return <ProductsContainer>
-    
-  </ProductsContainer>;
+	const [products, setProducts] = useState([]);
+
+  // runs on every render
+	useEffect(() => {
+		// corrects the window position
+		window.scroll({
+			left: 0,
+			top: 0,
+			behavior: 'auto',
+		});
+	}, []);
+
+	return (
+		<ProductsContainer>
+			<section className='upper'>
+				<h1>
+					Produtos <BiCollection />{' '}
+				</h1>
+			</section>
+			<section>
+				{products.length < 1 ? (
+					<article className='empty-message'>
+						<FaArchive />
+						<section>
+							<h2>Sem produtos na loja</h2>
+						</section>
+					</article>
+				) : null}
+			</section>
+		</ProductsContainer>
+	);
 };
 
 export default Products;
--- src/styles/admin/products.js
+++ src/styles/admin/products.js
@@ -1,6 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { empty_message_styles, propagateFlex } from './messages';
 
 export const ProductsContainer = styled.section`
-    min-height: 100vh;
+	min-height: 100vh;
 
-`
+	.empty-message {
+		${() => empty_message_styles}
+	}
+
+  .upper {
+		${() => propagateFlex}
+		gap: 10px;
+		max-width: 670px;
+
+		h1 {
+			position: relative;
+			line-height: 0;
+			padding-bottom: 12px;
+			svg {
+				position: absolute;
+				width: 30px;
+				height: 30px;
+				left: 160px;
+				top: -15px;
+			}
+		}
+	}
+`;
